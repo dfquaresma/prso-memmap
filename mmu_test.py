51725d855d2924bc19fa9c61ed7a3a204d7328d2
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-07-05 17:57:06 -0300

    Update mmu_test.py

--- mmu_test.py
+++ mmu_test.py
@@ -12,24 +12,24 @@ def linear_simple_page_fault_test():
   t3 = 4294967232 # 11111111111111111111 111111000000 # 0 page faults
   t4 = 4164943808 # 11111000001111111111 111111000000 # 1 page faults
   t5 = 4164943871 # 11111000001111111111 111111111111 # 0 page faults
-  l = MMU("linear")
-  mmu_output = l.map(t1)
+  linear = MMU("linear")
+  mmu_output = linear.map(t1)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
 
-  mmu_output = l.map(t2)
+  mmu_output = linear.map(t2)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
 
-  mmu_output = l.map(t3)
+  mmu_output = linear.map(t3)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 0)
   
-  mmu_output = l.map(t4)
+  mmu_output = linear.map(t4)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
   
-  mmu_output = l.map(t5)
+  mmu_output = linear.map(t5)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 0)
   print()
@@ -42,24 +42,24 @@ def hierarchical_simple_page_fault_test():
   t3 = 4294967232 # 1111111111 1111111111 111111000000 # 0 page faults
   t4 = 4164943808 # 1111100000 1111111111 111111000000 # 2 page faults
   t5 = 4164943871 # 1111100000 1111111111 111111111111 # 0 page faults
-  h = MMU("hierarchical")
-  mmu_output = h.map(t1)
+  hierarchical = MMU("hierarchical")
+  mmu_output = hierarchical.map(t1)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 2)
 
-  mmu_output = h.map(t2)
+  mmu_output = hierarchical.map(t2)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
   
-  mmu_output = h.map(t3)
+  mmu_output = hierarchical.map(t3)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 0)
   
-  mmu_output = h.map(t4)
+  mmu_output = hierarchical.map(t4)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 2)
   
-  mmu_output = h.map(t5)
+  mmu_output = hierarchical.map(t5)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 0)
   print()
@@ -72,24 +72,24 @@ def inverted_simple_page_fault_test():
   t3 = 18446185521737629695 # 1111111111111110000000 111111111111111100000111111111 111111111111 # 1 page faults
   t4 = 18446185521737629695 # 1111111111111110000000 111111111111111100000111111111 111111111111 # 0 page faults
   t5 = 18446744073709551584 # 1111111111111111111111 111111111111111111111111111111 111111100000 # 0 page faults
-  h = MMU("inverted")
-  mmu_output = h.map(t1)
+  inverted = MMU("inverted")
+  mmu_output = inverted.map(t1)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
 
-  mmu_output = h.map(t2)
+  mmu_output = inverted.map(t2)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
   
-  mmu_output = h.map(t3)
+  mmu_output = inverted.map(t3)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 1)
   
-  mmu_output = h.map(t4)
+  mmu_output = inverted.map(t4)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 0)
   
-  mmu_output = h.map(t5)
+  mmu_output = inverted.map(t5)
   print_mmu_output(mmu_output)
   assert (mmu_output[2] == 0)
   print()
@@ -98,3 +98,4 @@ if __name__ == '__main__':
   linear_simple_page_fault_test()
   hierarchical_simple_page_fault_test()
   inverted_simple_page_fault_test()
+  
